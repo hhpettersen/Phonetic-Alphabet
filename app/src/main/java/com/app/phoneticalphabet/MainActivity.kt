1d019f850202d90348593e5352439a90ba36cb28
Håkon Pettersen <helg.pett@gmail.com>
2022-09-25 14:00:46 +0200

    Add CoreApplication to manifest

--- app/src/main/java/com/app/phoneticalphabet/MainActivity.kt
+++ app/src/main/java/com/app/phoneticalphabet/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.app.phoneticalphabet.ui.navigation.graph.RootNavigationGraph
 import com.app.phoneticalphabet.ui.theme.MainTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
