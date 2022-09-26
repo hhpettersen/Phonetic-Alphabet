992a7beaed6b68f684a7c3d0c0bdb81180f5c74a
Håkon Pettersen <helg.pett@gmail.com>
2022-09-26 21:16:43 +0200

    Move QuizContent into separate file

---
+++ app/src/main/java/com/app/phoneticalphabet/ui/screens/quiz/QuizContent.kt
@@ -0,0 +1,60 @@
+package com.app.phoneticalphabet.ui.screens.quiz
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.phoneticalphabet.models.Answer
+import com.app.phoneticalphabet.models.Question
+
+@Composable
+fun QuizContent(
+    modifier: Modifier = Modifier,
+    state: QuizViewState,
+    onAnswerSelected: (Answer) -> Unit,
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "Current question: ${state.numberCurrentQuestion}/${state.numberOfQuestions}")
+            Text(text = "Score: ${state.score}")
+            Questions(
+                question = state.question,
+                onAnswerSelected = onAnswerSelected,
+                questionsEnabled = state.questionsEnabled,
+            )
+        }
+    }
+}
+
+@Composable
+fun Questions(
+    question: Question,
+    onAnswerSelected: (Answer) -> Unit,
+    questionsEnabled: Boolean,
+) {
+    question.answers.forEach {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            onClick = { onAnswerSelected(it) },
+            enabled = questionsEnabled
+        ) {
+            Text(text = it.word)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewQuizScreen() {
+    QuizScreen(
+        onEndGame = {}
+    )
+}
--- app/src/main/java/com/app/phoneticalphabet/ui/screens/quiz/QuizScreen.kt
+++ app/src/main/java/com/app/phoneticalphabet/ui/screens/quiz/QuizScreen.kt
@@ -1,40 +1,20 @@
 package com.app.phoneticalphabet.ui.screens.quiz
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.app.phoneticalphabet.models.Answer
-import com.app.phoneticalphabet.models.Question
 
 @Composable
 fun QuizScreen(
-    modifier: Modifier = Modifier,
     onEndGame: (Int) -> Unit,
 ) {
     val viewModel = hiltViewModel<QuizViewModel>()
-
     val state = viewModel.state.collectAsState()
 
-    Box(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier.align(Alignment.Center)
-        ) {
-            Text(text = "Current question: ${state.value.numberCurrentQuestion}/${state.value.numberOfQuestions}")
-            Text(text = "Score: ${state.value.score}")
-            Questions(
-                question = state.value.question,
-                onQuestionClicked = { viewModel.onAnswerSelected(it) },
-                questionsEnabled = state.value.questionsEnabled,
-            )
-        }
-    }
+    QuizContent(
+        state = state.value,
+        onAnswerSelected = { viewModel.onAnswerSelected(it) }
+    )
 
     state.value.uiEvent
         .firstOrNull()?.let { event ->
@@ -43,31 +23,4 @@ fun QuizScreen(
                 is UiEvent.EndQuiz -> onEndGame(event.finalScore)
             }
         }
-}
-
-@Composable
-fun Questions(
-    question: Question,
-    onQuestionClicked: (Answer) -> Unit,
-    questionsEnabled: Boolean,
-) {
-    question.answers.forEach {
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 32.dp),
-            onClick = { onQuestionClicked(it) },
-            enabled = questionsEnabled
-        ) {
-            Text(text = it.word)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewQuizScreen() {
-    QuizScreen(
-        onEndGame = {}
-    )
 }
