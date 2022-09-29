6c030dab88e13a33fdfc39a0f93211cee9cf497d
Håkon Pettersen <helg.pett@gmail.com>
2022-09-29 07:40:03 +0200

    Add new color-theme for API < 31

--- app/src/main/java/com/app/phoneticalphabet/ui/theme/Color.kt
+++ app/src/main/java/com/app/phoneticalphabet/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.app.phoneticalphabet.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val VardunGreen = Color(0xFF506600)
+val Finch = Color(0xFF5b6146)
+val William = Color(0xFF3a665e)
+val YellowGreen = Color(0xFFa9d467)
+val Coriander = Color(0xFFc0caac)
+val Sinbad = Color(0xFFa0d0ca)
--- app/src/main/java/com/app/phoneticalphabet/ui/theme/Theme.kt
+++ app/src/main/java/com/app/phoneticalphabet/ui/theme/Theme.kt
@@ -12,25 +12,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = YellowGreen,
+    secondary = Coriander,
+    tertiary = Sinbad
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = VardunGreen,
+    secondary = Finch,
+    tertiary = William
 )
 
 @Composable
