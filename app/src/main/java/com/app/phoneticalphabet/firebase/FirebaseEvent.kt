d92006e7799decbfecc177173715faebd0b8402e
Håkon Pettersen <helg.pett@gmail.com>
2022-10-13 13:56:57 +0200

    Add custom firebase-events

---
+++ app/src/main/java/com/app/phoneticalphabet/firebase/FirebaseEvent.kt
@@ -0,0 +1,12 @@
+package com.app.phoneticalphabet.firebase
+
+class FirebaseEvent {
+    companion object {
+        const val QUIZ_STARTED = "quiz_started"
+        const val QUIZ_FINISHED = "quiz_finished"
+        const val FLASHCARD_STARTED = "flashcard_started"
+        const val FLASHCARD_FINISHED = "flashcard_finished"
+        const val ALPHABET_VIEWED = "alphabet_viewed"
+        const val STATS_VIEWED = "stats_viewed"
+    }
+}
