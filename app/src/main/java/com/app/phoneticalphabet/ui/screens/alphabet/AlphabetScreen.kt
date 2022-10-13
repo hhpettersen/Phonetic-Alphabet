333f90e23dcbc5fc91d2c86896d990d4c483378b
Håkon Pettersen <helg.pett@gmail.com>
2022-10-13 14:11:48 +0200

    Log firebase-event when alphabet is viewed

--- app/src/main/java/com/app/phoneticalphabet/ui/screens/alphabet/AlphabetScreen.kt
+++ app/src/main/java/com/app/phoneticalphabet/ui/screens/alphabet/AlphabetScreen.kt
@@ -1,8 +1,27 @@
 package com.app.phoneticalphabet.ui.screens.alphabet
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.app.phoneticalphabet.firebase.FirebaseEvent
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.logEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @Composable
 fun AlphabetScreen() {
+    val viewModel = hiltViewModel<AlphabetViewModel>()
+
+    viewModel.logEvent()
     AlphabetContent()
+}
+
+@HiltViewModel
+class AlphabetViewModel @Inject constructor(
+    private val analytics: FirebaseAnalytics,
+) : ViewModel() {
+    fun logEvent() {
+        analytics.logEvent(FirebaseEvent.ALPHABET_VIEWED) {}
+    }
 }
