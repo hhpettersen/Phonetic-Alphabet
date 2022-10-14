35c7b3270dfd9ecae49fe20c2bcfe47e29e1f359
Håkon Pettersen <helg.pett@gmail.com>
2022-10-14 09:00:24 +0200

    Add PhonButton

--- app/src/main/java/com/app/phoneticalphabet/ui/components/StandardButton.kt
+++ app/src/main/java/com/app/phoneticalphabet/ui/components/StandardButton.kt
@@ -1,8 +1,12 @@
 package com.app.phoneticalphabet.ui.components
 
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,6 +29,34 @@ fun StandardButton(
         enabled = enabled,
     ) {
         Text(text = text)
+    }
+}
+
+@Composable
+fun PhonButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        modifier = modifier,
+        enabled = enabled,
+        shape = RoundedCornerShape(5.dp),
+        colors = colors,
+        onClick = onClick,
+        content = content
+    )
+}
+
+@Preview
+@Composable
+fun PreviewPhonButton() {
+    MainTheme {
+        PhonButton(onClick = { /*TODO*/ }) {
+            Text(text = "Test")
+        }
     }
 }
 
